e48b131e722d059e2186a09f87ce8d3918b8ae90
Michael Kotliar <misha.kotliar@gmail.com>
2018-06-28 12:21:47 -0400

    Update create_dag to skip all exceptions
    Prevents from failing if some job is not
    correct

--- cwl_airflow/create_dag.py
+++ cwl_airflow/create_dag.py
@@ -4,5 +4,10 @@ from cwl_airflow.utils.func import make_dag, get_active_jobs
 
 
 def create_dags():
-    return {job["dag_id"]: make_dag(job) for job in get_active_jobs(jobs_folder=configuration.get('cwl', 'jobs'),
-                                                                    limit=int(configuration.get('cwl', 'limit')))}
+    dags = {}
+    for job in get_active_jobs(jobs_folder=configuration.get('cwl', 'jobs'), limit=int(configuration.get('cwl', 'limit'))):
+        try:
+            dags[job["dag_id"]] = make_dag(job)
+        except Exception:
+            pass
+    return dags
