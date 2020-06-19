8951911107f327881369323d42c17f91098e2adc
Michael Kotliar <misha.kotliar@gmail.com>
2020-06-18 20:57:55 -0400

    Not important changes

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pytest -v --forked $DIR/test_helpers.py && \
 pytest -v --forked $DIR/test_parser.py && \
-pytest -v --forked $DIR/test_init_config.py
+pytest -v --forked $DIR/test_init_config.py && \
 pytest -v --forked $DIR/test_cwl.py
